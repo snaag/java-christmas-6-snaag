01a53a08f4eff8e777bbf08c33ac0bfa29dcc7fa
snaag <snaag.dev@gmail.com>
2023-11-12 19:45:49 +0900

    feat: (WeekdayDiscountService) 평일 할인 계산 로직 구현

---
+++ src/main/java/christmas/service/order/day/discount/WeekdayDiscountService.java
@@ -0,0 +1,14 @@
+package christmas.service.order.day.discount;
+
+import static christmas.utils.Constants.SPECIAL;
+import static christmas.utils.Constants.WEEKDAY;
+
+public class WeekdayDiscountService extends DiscountService {
+    @Override
+    public boolean isDiscountDay(int day) {
+        if(super.calendar[day] == WEEKDAY) {
+            return true;
+        }
+        return super.calendar[day] == WEEKDAY + SPECIAL;
+    }
+}
