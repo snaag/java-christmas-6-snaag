92198be43dea2bd1c86245fa35782f21191ce0be
snaag <snaag.dev@gmail.com>
2023-11-12 19:13:56 +0900

    refactor: (DayInputService) 에러 시 입력 무한 로직 DayService 에서 분리

---
+++ src/main/java/christmas/service/order/day/DayInputService.java
@@ -0,0 +1,39 @@
+package christmas.service.order.day;
+
+import christmas.dto.DayInputDto;
+import christmas.utils.day.DayValidations;
+import christmas.view.InputView;
+
+public class DayInputService {
+    private final DayInputDto dayInputDto;
+
+    public DayInputService() {
+        this.dayInputDto = new DayInputDto();
+    }
+
+    public int setDay() {
+        while(!dayInputDto.isValid()) {
+            this.checkValidDateOrError();
+        }
+
+        return Integer.parseInt(this.dayInputDto.getDay());
+    }
+
+    private void checkValidDateOrError() {
+        try {
+            String day = InputView.readDate();
+            this.validateDate(day);
+            dayInputDto.setValid(true);
+            dayInputDto.setDay(day);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void validateDate(String givenDay) {
+        DayValidations.checkNumber(givenDay);
+
+        int day = Integer.parseInt(givenDay);
+        DayValidations.checkInMonthRange(day);
+    }
+}
