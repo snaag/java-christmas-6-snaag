dd830f0ee54f9f3e0ac1d67cbe94b1c4b78c9d4f
snaag <snaag.dev@gmail.com>
2023-11-12 19:45:03 +0900

    refactor: (DiscountService) DayService 에 있던 할인 날짜 계산을 DiscountService 로 분리

---
+++ src/main/java/christmas/service/order/day/discount/DiscountService.java
@@ -0,0 +1,29 @@
+package christmas.service.order.day.discount;
+
+import static christmas.utils.Constants.*;
+
+public abstract class DiscountService {
+    protected byte[] calendar;
+
+    public DiscountService() {
+        this.makeCalendar();
+    }
+
+    private void makeCalendar() {
+        this.calendar = new byte[]{
+                0,                                      WEEKEND,WEEKEND, // 1~2
+                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 3~9
+                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 10~16
+                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 17~23
+                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 24~30
+                WEEKDAY                                                  // 31
+        };
+
+        byte[] specialDays = new byte[]{3, 10, 17, 24, 25, 31};
+        for(byte specialDay: specialDays) {
+            this.calendar[specialDay] += SPECIAL;
+        }
+    }
+
+    public abstract boolean isDiscountDay(int day);
+}
