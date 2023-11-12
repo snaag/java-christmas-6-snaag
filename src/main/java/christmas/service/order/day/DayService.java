3bef558181388262ea02579a98bcc33378768b11
snaag <snaag.dev@gmail.com>
2023-11-12 19:44:09 +0900

    refactor: (DayService) 할인 날짜 계산을 DiscountService 로 분리

--- src/main/java/christmas/service/order/day/DayService.java
+++ src/main/java/christmas/service/order/day/DayService.java
@@ -1,52 +1,21 @@
 package christmas.service.order.day;
 
-import static christmas.utils.Constants.*;
+import christmas.service.order.day.input.DayInputService;
 
 public class DayService {
+
+    private final DayInputService dayInputService;
     private int day;
-    private byte[] calendar;
 
     public DayService() {
-        this.makeCalendar();
+        this.dayInputService = new DayInputService();
     }
 
-    public void makeCalendar() {
-        this.calendar = new byte[]{
-                0,                                      WEEKEND,WEEKEND, // 1~2
-                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 3~9
-                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 10~16
-                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 17~23
-                WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKDAY,WEEKEND,WEEKEND, // 24~30
-                WEEKDAY                                                  // 31
-        };
-
-        byte[] specialDays = new byte[]{3, 10, 17, 24, 25, 31};
-        for(byte specialDay: specialDays) {
-            this.calendar[specialDay] += SPECIAL;
-        }
+    public void setDay() {
+        this.day = this.dayInputService.setDay();
     }
-
     public int getDay() {
         return this.day;
     }
 
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public boolean isWeekday(int day) {
-        if(this.calendar[day] == WEEKDAY) {
-            return true;
-        }
-
-        return this.calendar[day] == WEEKDAY + SPECIAL;
-    }
-
-    public boolean isWeekend(int day) {
-        return this.calendar[day] == WEEKEND;
-    }
-
-    public boolean isSpecialDay(int day) {
-        return this.calendar[day] == WEEKDAY + SPECIAL;
-    }
 }
