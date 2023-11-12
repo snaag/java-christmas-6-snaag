f7e467e75ddb0a6eafc7cfff8edf9694c13d2df7
snaag <snaag.dev@gmail.com>
2023-11-13 02:59:47 +0900

    feat: (MenuValidations) 누락된 메뉴 유효성 검증 로직 구현 - 전체 주문 카운트가 20이 넘으면 안됨 - 음료만 주문하면 안됨

--- src/main/java/christmas/utils/menu/MenuValidations.java
+++ src/main/java/christmas/utils/menu/MenuValidations.java
@@ -44,4 +44,28 @@ public class MenuValidations {
             throw new IllegalArgumentException(MenuMessage.NOT_VALID.getMessage());
         }
     }
+
+    public static void checkTotalCountMoreThan(String[] menus, int threshold) {
+        int total = Arrays.stream(menus)
+                .map((menu) -> menu.split(COUNT_SEPARATOR)[1])
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
+
+        if(total > threshold) {
+            throw new IllegalArgumentException(MenuMessage.NOT_VALID.getMessage());
+        }
+    }
+
+    public static void checkOnlyBeverage(String[] menus) {
+        long countNotBeverage =  Arrays.stream(menus)
+                .map((menu) -> menu.split(COUNT_SEPARATOR)[0])
+                .map(Menu::getMenu)
+                .map((menu) -> menu.getCategory())
+                .filter((category) -> !category.equals(Category.BEVERAGE))
+                .count();
+
+        if(countNotBeverage == 0) {
+            throw new IllegalArgumentException(MenuMessage.NOT_VALID.getMessage());
+        }
+    }
 }
