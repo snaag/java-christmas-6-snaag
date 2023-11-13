20c2d342f89c4266d2076733b388db2cbb2a2e8a
snaag <snaag.dev@gmail.com>
2023-11-13 12:52:57 +0900

    refactor: (OrderServiceTest) 리팩토링에 따른 총계 계산 로직 테스트 삭제

--- src/test/java/christmas/service/order/OrderServiceTest.java
+++ src/test/java/christmas/service/order/OrderServiceTest.java
@@ -14,33 +14,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 class OrderServiceTest {
     static OrderService orderService;
 
-    @BeforeAll
-    public static void prepare() {
-        orderService = new OrderService();
-    }
-
-    @MethodSource("providedMenus")
-    @ParameterizedTest
-    void 총계_출력(HashMap<Menu, Integer> menus, int expectedPrice) {
-        assertThat(expectedPrice)
-                .isEqualTo(orderService.getTotalPrice(menus));
-
-    }
-
-    static Stream<Arguments> providedMenus() {
-        return Stream.of(
-                Arguments.of(new HashMap<Menu, Integer>() {{
-                    put(Menu.MUSHROOM_SOUP, 2);
-                    put(Menu.CHRISTMAS_PASTA, 1);
-                    put(Menu.TBONE_STEAK, 1);
-                    put(Menu.ZERO_COKE, 1);
-                }}, 95000),
-                Arguments.of(new HashMap<Menu, Integer>() {{
-                    put(Menu.SEAFOOD_PASTA, 2);
-                    put(Menu.RED_WINE, 1);
-                    put(Menu.CHOCOLATE_CAKE, 1);
-                }}, 145000)
-        );
-    }
 
 }
