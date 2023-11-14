835b7c9d5b94dd2e70d4057aa17d3e4c8ace9874
snaag <snaag.dev@gmail.com>
2023-11-15 01:41:33 +0900

    refactor: (SpecialdayDiscountPolicyService) 불필요한 인스턴스 변수 -> 지역 변수

--- src/main/java/christmas/service/checkstand/benefit/discount/WeekdayDiscountPolicyService.java
+++ src/main/java/christmas/service/checkstand/benefit/discount/WeekdayDiscountPolicyService.java
@@ -9,11 +9,6 @@ import static christmas.utils.Constants.SPECIAL;
 import static christmas.utils.Constants.WEEKDAY;
 
 public class WeekdayDiscountPolicyService extends DiscountPolicyService {
-
-    String DISCOUNT_NAME = "평일 할인";
-
-    int DISCOUNT_AMOUNT = 2_023;
-
     @Override
     public boolean isDiscountDay(int day) {
         if(super.calendar[day] == WEEKDAY) {
@@ -22,9 +17,10 @@ public class WeekdayDiscountPolicyService extends DiscountPolicyService {
         return super.calendar[day] == WEEKDAY + SPECIAL;
     }
 
-    // TODO 평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인  (동일 메뉴가 여러개면 여러번 할인)
     @Override
     public int getDiscountPrice(int day, HashMap<Menu, Integer> menus) {
+        int DISCOUNT_AMOUNT = 2_023;
+
         if(!isDiscountDay(day)) {
             return 0;
         }
@@ -39,7 +35,7 @@ public class WeekdayDiscountPolicyService extends DiscountPolicyService {
 
     @Override
     public String getDiscountName() {
-        return this.DISCOUNT_NAME;
+        return "평일 할인";
     }
 
     @Override
