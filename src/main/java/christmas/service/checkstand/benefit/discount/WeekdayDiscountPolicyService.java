e63ce882618b3a947545cdaa0f3c7259f765c155
snaag <snaag.dev@gmail.com>
2023-11-14 23:17:13 +0900

    refactor: (WeekdayDiscountPolicyService) Policy 붙여줌

--- src/main/java/christmas/service/checkstand/benefit/discount/WeekdayDiscountPolicyService.java
+++ src/main/java/christmas/service/checkstand/benefit/discount/WeekdayDiscountPolicyService.java
@@ -3,7 +3,12 @@ package christmas.service.counter.discount;
 import static christmas.utils.Constants.SPECIAL;
 import static christmas.utils.Constants.WEEKDAY;
 
-public class WeekdayDiscountService extends DiscountService {
+public class WeekdayDiscountPolicyService extends DiscountPolicyService {
+
+    String DISCOUNT_NAME = "평일 할인";
+
+    int DISCOUNT_AMOUNT = 2_023;
+
     @Override
     public boolean isDiscountDay(int day) {
         if(super.calendar[day] == WEEKDAY) {
