5dbcf4485191162c76facc9c75bc752f45661782
snaag <snaag.dev@gmail.com>
2023-11-14 23:35:43 +0900

    feat: (SpecialdayDiscountPolicyService) 특별 할인 로직 구현

--- src/main/java/christmas/service/checkstand/benefit/discount/SpecialdayDiscountPolicyService.java
+++ src/main/java/christmas/service/checkstand/benefit/discount/SpecialdayDiscountPolicyService.java
@@ -1,5 +1,8 @@
-package christmas.service.counter.discount;
 package christmas.service.checkstand.benefit.discount;
+
+import christmas.utils.menu.Menu;
+
+import java.util.HashMap;
 
 import static christmas.utils.Constants.*;
 
@@ -11,5 +14,24 @@ public class SpecialdayDiscountPolicyService extends DiscountPolicyService {
     @Override
     public boolean isDiscountDay(int day) {
         return super.calendar[day] == WEEKDAY + SPECIAL;
+    }
+
+
+    @Override
+    public int getDiscountPrice(int day) {
+        if(!this.isDiscountDay(day)) {
+            return 0;
+        }
+        return DISCOUNT_AMOUNT;
+    }
+
+    @Override
+    public String getDiscountName() {
+        return this.DISCOUNT_NAME;
+    }
+
+    @Override
+    public int getDiscountPrice(int day, HashMap<Menu, Integer> menus) {
+        return 0;
     }
 }
