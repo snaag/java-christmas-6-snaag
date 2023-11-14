d0531f76d8a92ebce93904d821963a0c035ca2e9
snaag <snaag.dev@gmail.com>
2023-11-14 23:35:54 +0900

    feat: (WeekdayDiscountPolicyService) 평일 할인 로직 구현

--- src/main/java/christmas/service/checkstand/benefit/discount/WeekdayDiscountPolicyService.java
+++ src/main/java/christmas/service/checkstand/benefit/discount/WeekdayDiscountPolicyService.java
@@ -1,5 +1,9 @@
-package christmas.service.counter.discount;
 package christmas.service.checkstand.benefit.discount;
+
+import christmas.utils.menu.Category;
+import christmas.utils.menu.Menu;
+
+import java.util.HashMap;
 
 import static christmas.utils.Constants.SPECIAL;
 import static christmas.utils.Constants.WEEKDAY;
@@ -17,4 +21,30 @@ public class WeekdayDiscountPolicyService extends DiscountPolicyService {
         }
         return super.calendar[day] == WEEKDAY + SPECIAL;
     }
+
+    // TODO 평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인  (동일 메뉴가 여러개면 여러번 할인)
+    @Override
+    public int getDiscountPrice(int day, HashMap<Menu, Integer> menus) {
+        if(!isDiscountDay(day)) {
+            return 0;
+        }
+
+        int dessertMenuCount = menus.keySet().stream()
+                .filter((menu) -> menu.getCategory().equals(Category.DESSERT))
+                .map(menus::get)
+                .reduce(0, Integer::sum);
+
+        return dessertMenuCount*DISCOUNT_AMOUNT;
+    }
+
+    @Override
+    public String getDiscountName() {
+        return this.DISCOUNT_NAME;
+    }
+
+    @Override
+    public int getDiscountPrice(int day) {
+        return 0;
+    }
+
 }
