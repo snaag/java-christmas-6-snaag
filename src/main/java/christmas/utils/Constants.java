c71c7155a3975c403b5998638cc2eddee5c3b73e
snaag <snaag.dev@gmail.com>
2023-11-15 01:27:00 +0900

    feat: (Constants) 상수값 작성

---
+++ src/main/java/christmas/utils/Constants.java
@@ -0,0 +1,18 @@
+package christmas.utils;
+
+public class Constants {
+    public static String ERROR_PREFIX = "[ERROR]";
+
+    public static int DAY_UNDER_BOUND = 1;
+    public static int DAY_UPPER_BOUND = 31;
+    public static int DAY_CHRISTMAS = 25;
+    public static byte WEEKDAY = 1;
+    public static byte WEEKEND = 2;
+    public static byte SPECIAL = 4;
+
+    public static int PRESENT_COUNT = 1;
+
+    public static String SEPARATOR = ",";
+    public static String COUNT_SEPARATOR = "-";
+
+}
