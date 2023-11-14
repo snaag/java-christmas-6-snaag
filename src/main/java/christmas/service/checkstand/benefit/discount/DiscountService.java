f97da578b83a9afbc639b7637567cb8c0d1a688b
snaag <snaag.dev@gmail.com>
2023-11-15 00:10:05 +0900

    feat: (DiscountService) 날짜와 메뉴에 따라 할인 반환하는 로직 구현

---
+++ src/main/java/christmas/service/checkstand/benefit/discount/DiscountService.java
@@ -0,0 +1,34 @@
+package christmas.service.checkstand.benefit.discount;
+
+import christmas.utils.menu.Menu;
+import java.util.HashMap;
+
+public class DiscountService {
+    private final ChristmasDiscountPolicyService christmasService;
+    private final SpecialdayDiscountPolicyService specialdayService;
+    private final WeekdayDiscountPolicyService weekdayService;
+    private final WeekendDiscountPolicyService weekendService;
+
+    public DiscountService() {
+        this.christmasService = new ChristmasDiscountPolicyService();
+        this.specialdayService = new SpecialdayDiscountPolicyService();
+        this.weekdayService = new WeekdayDiscountPolicyService();
+        this.weekendService = new WeekendDiscountPolicyService();
+    }
+
+    public HashMap<String, Integer> getDiscountHistory(HashMap<Menu, Integer> menus, int day) {
+        HashMap<String, Integer> discountHistory = new HashMap<>();
+
+        if(this.christmasService.getDiscountPrice(day) > 0)
+            discountHistory.put(this.christmasService.getDiscountName(), this.christmasService.getDiscountPrice(day));
+        if(this.weekdayService.getDiscountPrice(day, menus) > 0)
+            discountHistory.put(this.weekdayService.getDiscountName(), this.weekdayService.getDiscountPrice(day, menus));
+        if(this.weekendService.getDiscountPrice(day, menus) > 0)
+            discountHistory.put(this.weekendService.getDiscountName(), this.weekendService.getDiscountPrice(day, menus));
+        if(this.specialdayService.getDiscountPrice(day) > 0)
+            discountHistory.put(this.specialdayService.getDiscountName(), this.specialdayService.getDiscountPrice(day));
+
+        return discountHistory;
+    }
+
+}
